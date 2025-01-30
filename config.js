1b57fb0e4822c4881079a394c2ddddfbf9565972
whitecoolman <miraclerose0303@gmail.com>
2025-01-30 06:22:36 +0200

    config
    Co-Authored-By: Darksoul0814 <190168566+darksoul0814@users.noreply.github.com>

--- config.js
+++ config.js
@@ -1,2 +1,16 @@
 require("dotenv").config();
 const { Pinecone } = require("@pinecone-database/pinecone");
+const axios = require("axios");
+
+const OPEN_API_KEY = process.env.OPEN_API_KEY;
+const PINECONE_API_KEY = process.env.PINECONE_API_KEY;
+const PINECONE_ENV = process.env.PINECONE_ENV;
+const PINECONE_INDEX = process.env.PINECONE_INDEX;
+
+const pinecone = new Pinecone({
+  apikey: PINECONE_API_KEY,
+  environment: PINECONE_ENV,
+});
+
+const index = pinecone.index(PINECONE_INDEX);
+module.exports = { OPEN_API_KEY, index };

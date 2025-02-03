38d992b8a0cfcdc55f60dd660fbc67f07fc4c732
whitecoolman <miraclerose0303@gmail.com>
2025-02-03 08:36:28 +0200

    funciton

--- src/config/config.js
+++ src/config/config.js
@@ -6,13 +6,12 @@ const OPEN_API_KEY = process.env.OPEN_API_KEY;
 const PINECONE_API_KEY = process.env.PINECONE_API_KEY;  
 const PINECONE_ENV = process.env.PINECONE_ENV;  
 const PINECONE_INDEX = process.env.PINECONE_INDEX;  
-console.log(process.env);
 // Initialize Pinecone with error handling  
 let pinecone;  
 try {  
   pinecone = new Pinecone({  
-    apiKey: "pcsk_2Cawxu_37SYGko3PW8rU61ErXKDRK6dFoY78tGn4FYE9WH71hKbFNdaB8HN7ExvJE1j56r",  
-    environment: "us-west1-gcp",  
+    apiKey: PINECONE_API_KEY
+    // environment: PINECONE_ENV,  
   });  
 } catch (error) {  
   console.error("Failed to initialize Pinecone:", error.message);  
